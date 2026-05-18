009c5280613ef6bc706f769c1ae5ee68f3283bbf
harish.vk <harish.vk@zohocorp.com>
2026-05-18 13:06:38 +0530

    style changes

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   // Paste your deployed Google Apps Script Web App URL here
   // See README.md for instructions
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxLb1nOgEyWE0DnihnMYuKQupfBDX4lAW_uSi3Bkbg1IlxUfWQ6OW9rwg1BgKjkBJ7D/exec',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwv8qssFUIA0aUDSDCrQ1CBWbVHM-Y0nFMO5ViruYshTJFJXuxYfu240Vb8MRsrIVPO/exec',
 
   APP_NAME: 'VaultZero',
   PAGE_SIZE: 10,
